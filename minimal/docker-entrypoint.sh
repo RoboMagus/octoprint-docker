496eb248a62f4ecc49363bfe8f60037fed804e38
RoboMagus <68224306+RoboMagus@users.noreply.github.com>
2022-06-28 20:58:34 +0200

    Just fillers...

--- minimal/docker-entrypoint.sh
+++ minimal/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+
+VERSION_FILE='/octoprint/octoprint/.CONTAINER_VERSION'
+
+# Test if file exists
+# If yes, check if contents equal current version
+# If either are NO, run pip check...
+
+pip check
+
+
 if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
   set -- octoprint --basedir /octoprint/octoprint "$@"
 fi
